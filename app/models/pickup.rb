57d207b316644fed11978fd066a375294d9cc36a
Angel Calvo <polloverdetm@gmail.com>
2014-07-29 18:03:00 +0200

    Location complete validation

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -1,7 +1,14 @@
 class Pickup < ActiveRecord::Base
 	validates :date, :picker, :coin, presence: true
+  validate :location_complete
 
 	belongs_to :wallet
   belongs_to :coin
   belongs_to :picker, class_name: "User"
+
+  def location_complete
+    if @latitude.nil? ^ @longitude.nil?
+      errors.add("Location is incomplete")
+    end
+  end
 end
--- test/models/pickup_test.rb
+++ test/models/pickup_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class PickupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "location complete" do
+    pickup = pickups(:one)
+    pickup.latitude = -34.3423423
+    assert pickup.invalid?
+  end
 end
