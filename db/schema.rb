06a1000475e560763a0ad2fbd7d9023781ba9f17
Angel Calvo <polloverdetm@gmail.com>
2014-08-01 00:06:52 +0200

    Location attribute added to Pickup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726103723) do
+ActiveRecord::Schema.define(version: 20140731204922) do
 
   create_table "coins", force: true do |t|
     t.integer  "value"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20140726103723) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "picker_id"
+    t.string   "location"
   end
 
   create_table "users", force: true do |t|
