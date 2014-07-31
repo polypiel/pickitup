ad0b73c4c7959cb774130733bd456f377be80e0d
Angel Calvo <polloverdetm@gmail.com>
2014-08-01 00:02:10 +0200

    Code format

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,6 +1,6 @@
 class Coin < ActiveRecord::Base
   validates :currency, :value, :name, :short_name, presence: true
-  validates :value, numericality: {greater_than_or_equal_to: 1}
+  validates :value, numericality: { greater_than_or_equal_to: 1 }
   
   belongs_to :currency
 
