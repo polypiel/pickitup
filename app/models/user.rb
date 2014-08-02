7d5a9b8f1d179e531da597e9c95a322f39bf7f69
Angel Calvo <polloverdetm@gmail.com>
2014-08-02 16:44:25 +0200

    Username not mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :username, :email, presence: true
+  validates :email, presence: true
   validates :username, :email, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
