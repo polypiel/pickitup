821c23d0b0220cfdfe0a6d1ba6d4e888f9fb50ca
Angel Calvo <polloverdetm@gmail.com>
2014-08-03 23:25:00 +0200

    Welcome controller erased

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Mail configuration
+# Depot::Application.configure do
+#   config.action_mailer.delivery_method = :smtp
+
+#   config.action_mailer.smtp_settings = {
+#     address: "",
+#     port: ,
+#     domain: "",
+#     authentication: "plain",
+#     user_name: "",
+#     password: "",
+#     enable_starttls_auto: true
+#   }
+# end
