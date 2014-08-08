afc22b19575122728476043fe693f712b4882b66
Angel Calvo <polloverdetm@gmail.com>
2014-08-09 00:06:38 +0200

    Schema creation migration

---
+++ db/migrate/20140808213640_setup.rb
@@ -0,0 +1,48 @@
+class Setup < ActiveRecord::Migration
+  def change
+    drop_table :currencies
+    create_table :currencies do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    drop_table :coins
+    create_table :coins do |t|
+      t.integer :value
+      t.string :name
+      t.string :short_name
+      t.belongs_to :currency
+      t.timestamps
+    end
+
+    drop_table :wallets
+    create_table :wallets do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    drop_table :pickups
+    create_table :pickups do |t|
+      t.datetime :picked_at
+      t.string :comments
+      t.decimal :longitude
+      t.decimal :latitude
+      t.string :location
+      t.belongs_to :coin
+      t.belongs_to :wallet
+      t.belongs_to :picker
+      t.timestamps
+    end
+
+    drop_table :users
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.integer :role
+      t.boolean :active
+      t.string :password_digest
+      t.belongs_to :wallet
+      t.timestamps
+    end
+  end
+end
