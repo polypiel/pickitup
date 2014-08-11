5e76c5ab09bbbe1289ea5d531e0b7e57fd689c82
Angel Calvo <polloverdetm@gmail.com>
2014-08-11 18:38:47 +0200

    User creation fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 
 euro = Currency.create(name: 'Euro')
 
@@ -15,5 +10,6 @@ Coin.create(value: 1, name: "céntimo", short_name: "cént", currency: euro)
 Coin.create(value: 2, name: "céntimo", short_name: "cént", currency: euro)
 Coin.create(value: 5, name: "céntimo", short_name: "cént", currency: euro)
 
-marielos = Wallet.create(name: "Marielo's wallet")
-marielo = User.create(username: "Marielo", email: "marielo@email.com", role: 1, wallet: marielos, active: true, BCrypt::Password.create('marielo'))
+marielos = Wallet.create(name: "Marielos wallet")
+
+marielo = User.create(username: "Marielo", email: "marielo@email.com", role: 1, wallet: marielos, active: true, password_digest: BCrypt::Password.create('marielo'))
