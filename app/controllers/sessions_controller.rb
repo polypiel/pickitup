f2f2529b747e7eb0e9e8f53258a15e73cd96008f
Angel Calvo <polloverdetm@gmail.com>
2014-08-13 18:46:48 +0200

    Logged user's wallet id is stored in the session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user and user.active and user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:wallet_id] = user.wallet_id
       redirect_to pickups_url
     else
       redirect_to login_url, alert: "Invalid user/password"
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:wallet_id] = nil
     redirect_to login_url, notice: "Logged out"
   end
 end
