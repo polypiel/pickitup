3acf016a5b8b3506b10b7655d888023e0bd276f8
Angel Calvo <polloverdetm@gmail.com>
2014-09-10 15:44:51 +0200

    Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,15 @@ class DashboardController < ApplicationController
   def index
     @coins = Pickup.count
     @money = Pickup.joins(:coin).sum(:value)
+
+    @top_users = ActiveRecord::Base.connection.execute('
+      SELECT u.username, count(u.id) AS coins , sum(c.value) AS value
+      FROM users u
+      JOIN pickups p ON p.picker_id = u.id
+      JOIN coins c ON p.coin_id = c.id
+      GROUP BY u.id
+      ORDER BY coins DESC
+      LIMIT 3
+    ')
   end
 end
