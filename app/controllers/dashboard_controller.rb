84612710597ea4ace6cc0b58061b0665d8f60c76
Angel Calvo <polloverdetm@gmail.com>
2014-09-17 16:51:31 +0200

    Only current wallet stats shown

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,20 @@
 class DashboardController < ApplicationController
   def index
-    @coins = Pickup.count
-    @money = Pickup.joins(:coin).sum(:value)
+    wallet_id = session[:wallet_id]
 
-    @top_users = ActiveRecord::Base.connection.execute('
+    @coins = Pickup.where(wallet_id: wallet_id).count
+    @money = Pickup.where(wallet_id: wallet_id).joins(:coin).sum(:value)
+
+    @top_users = ActiveRecord::Base.connection.execute("
       SELECT u.id, u.username, count(u.id) AS coins , sum(c.value) AS value
       FROM users u
       JOIN pickups p ON p.picker_id = u.id
       JOIN coins c ON p.coin_id = c.id
+      WHERE u.wallet_id = #{wallet_id}
       GROUP BY u.id
       ORDER BY coins DESC
       LIMIT 3
-    ')
+    ")
 
     @last_pickups = Pickup.where(wallet_id: session[:wallet_id], picked_at: (15.days.ago.to_date)..(Time.zone.now)).order(picked_at: :desc).limit(3)
   end
