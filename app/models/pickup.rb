85ca0502f1de4e60ffcc40b410648de063444236
Angel Calvo <polloverdetm@gmail.com>
2014-11-23 23:21:27 +0100

    better naming

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -2,7 +2,7 @@ class Pickup < ActiveRecord::Base
 	validates :picked_at, :picker, :coin, presence: true
   validates :longitude, numericality: { greater_than: -180, less_than: 180 }, allow_nil: true
   validates :latitude, numericality: { greater_than: -90, less_than: 90 }, allow_nil: true
-  validate :location_complete, :wallet_owner
+  validate :location_complete?, :wallet_owner
 
 	belongs_to :wallet
   belongs_to :coin
@@ -10,11 +10,11 @@ class Pickup < ActiveRecord::Base
 
   date_time_attribute :picked_at
 
-  def has_location
-    not latitude.nil? or not longitude.nil?
+  def has_coordinates?
+    not latitude.nil? and not longitude.nil?
   end
 
-  def location_complete
+  def location_complete?
     if latitude.nil? ^ longitude.nil?
       errors.add(:latitude, "Location is incomplete")
     end
