479c48da10029e547e8eb3b2d005f9906f850261
Angel Calvo <polloverdetm@gmail.com>
2015-01-10 19:22:56 +0100

    script to load unregiestered pickups

---
+++ db/seeds/angel_unregistered_2014.rb
@@ -0,0 +1,12 @@
+# Coins handed in but not registered
+# "2014-09-12" "2014-12-12" "2014-12-24" "2014-11-23" "2014-09-27" "2014-12-17" "2014-11-28"
+
+angel_id = Rails.env.development? ? 20 : 10
+
+Pickup.create(picked_at: "2014-09-12 12:00:00.0", location: "MaRails.env.development?drid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-09-12 12:00:00.0", updated_at: "2014-09-12 12:00:00.0")
+Pickup.create(picked_at: "2014-12-12 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-12-12 12:00:00.0", updated_at: "2014-12-12 12:00:00.0")
+Pickup.create(picked_at: "2014-12-24 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-12-24 12:00:00.0", updated_at: "2014-12-24 12:00:00.0")
+Pickup.create(picked_at: "2014-11-23 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-11-23 12:00:00.0", updated_at: "2014-11-23 12:00:00.0")
+Pickup.create(picked_at: "2014-09-27 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-09-27 12:00:00.0", updated_at: "2014-09-27 12:00:00.0")
+Pickup.create(picked_at: "2014-12-17 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-12-17 12:00:00.0", updated_at: "2014-12-17 12:00:00.0")
+Pickup.create(picked_at: "2014-11-28 12:00:00.0", location: "Madrid, Spain", coin_id: 1, wallet_id: 1, picker_id: angel_id, created_at: "2014-11-28 12:00:00.0", updated_at: "2014-11-28 12:00:00.0")
