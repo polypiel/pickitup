812ad4b34f32f44ce5e3e29d2073a09816db13cf
Angel Calvo <polloverdetm@gmail.com>
2015-06-23 23:49:46 +0200

    handed_over load script

--- db/seeds/hand_over_old_pickups.rb
+++ db/seeds/hand_over_old_pickups.rb
@@ -4,7 +4,7 @@ Pickup.where("picked_at <= ?", "2014-12-31 00:00:00.0").each do |p|
   p.save
 end
 
-Pickup.where(picker_id:, 1).each do |p|
+Pickup.where(picker_id: 1).each do |p|
   p.handed_over = true
   p.save
 end
