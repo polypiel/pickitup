61ee8c63e3d9da544720e1374a3f09e438f1fea6
Angel Calvo <polloverdetm@gmail.com>
2016-06-16 00:34:52 +0200

    Fix coin list in stats page

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
-ruby "2.1.5"
+ruby "2.3.0"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5'
+gem 'rails', '4.2.6'
 # Databases for Active Record
 group :production do
   gem 'pg'
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,13 +5,13 @@ class StatsController < ApplicationController
     @year = (params['year'] || @years.max).to_i
 
     @coins_by_type = ActiveRecord::Base.connection.execute("
-      SELECT c.value AS coin, count(c.id) AS amount
+      SELECT c.value AS coin, c.short_name AS currency, count(c.id) AS amount
       FROM pickups p
       JOIN coins c ON p.coin_id = c.id
       JOIN users u ON p.picker_id = u.id
       WHERE u.wallet_id = #{wallet_id} AND p.year = #{@year}
       GROUP BY c.id
-      ORDER BY c.value ASC
+      ORDER BY c.currency_id, c.value ASC
     ")
 
     @top_users = ActiveRecord::Base.connection.execute("
