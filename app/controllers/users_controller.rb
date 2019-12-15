6228d53de77054037101ab59f77187dda2f5097e
Angel <angeluiscalvo@gmail.com>
2019-12-15 21:41:55 +0100

    Yearly chart moved to profile fix~

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def show
     @current_user = @user.id == session[:user_id]
 
+    @years = Pickup.pluck('DISTINCT year').sort.reverse
     wallet_id = get_logged_user.wallet.id
     all = Pickup.where(wallet_id: wallet_id)
     @pickups_by_year = year_pickups all #all.group(:year).count
